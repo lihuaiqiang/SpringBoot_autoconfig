70aa57e3ede7c9d5154720d7ee09d994cb1993f0
lihuaiqiang <314207558@qq.com>
2018-07-15 21:31:35 +0800

    add

--- src/main/java/com/aixuexi/springboot/SpringBoot02ConfigAutoconfigApplication.java
+++ src/main/java/com/aixuexi/springboot/SpringBoot02ConfigAutoconfigApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * 注解  （github 测试原项目）  
+ * 注解  （github 测试原项目aa）
  */
 @SpringBootApplication
 public class SpringBoot02ConfigAutoconfigApplication {
